71772d0ce531f1c85d8269272595cf5cc9b5b93a
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-08-21 17:44:48 +0500

    favicon changes

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('front/src/images/favicon.png') }}"/>
-    <link rel="icon"type="image/png"sizes="16x16" href="{{ asset('front/src/images/favicon.png') }}"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('front/images/favicon/apple-touch-icon.png') }}"> 
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('front/src/images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('front/src/images/favicon/favicon-16x16.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
     <link href="{{ asset('front/src/css/output.css') }}" rel="stylesheet" />
     <link href="{{ asset('front/src/css/custom.css') }}" rel="stylesheet" />
